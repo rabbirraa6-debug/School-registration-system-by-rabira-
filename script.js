8c82a5821bb4c15aedb263b9413581382654ed2e
Rabira <rabbirraa6@gmail.com>
2026-09-01 18:53:22 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuButton = document.getElementById("menuButton");
-const mainNav = document.querySelector(".main-nav");
-
-if (menuButton && mainNav) {
-  menuButton.addEventListener("click", () => {
-    mainNav.classList.toggle("mobile-open");
-  });
-}
